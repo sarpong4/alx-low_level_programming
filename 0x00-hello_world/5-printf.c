37e1341863970313938100817f6220ea66164760
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-08 08:34:58 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+/* printing */
+int main(void)
+{
+	/* main function */
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
