63c3ea1ad2b5f0a36500ed4932454d9f1a81b0a1
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-08 09:31:36 -0700

    error-stderr1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
+/* main */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
