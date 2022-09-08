c7fe1aaf13e90c21adf43f87668e7e289e473470
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-08 09:17:21 -0700

    errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 int main(void)
 {
 	/* main function */
-	fprintf(stderr, "%s" "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
