61cd64db64e4ee5b89bac9c8f45f3488dceba8cf
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-09 22:25:56 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,12 +13,12 @@ int main(void)
 	i = 0;
 	while (i <= 99)
 	{
-	       	j = 0;
+		j = 0;
 		while (j <= 99)
 		{
 			if (i < j && i != j)
 			{
-	  			putchar((i / 10) + '0');
+				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
 				putchar((j / 10) + '0');
