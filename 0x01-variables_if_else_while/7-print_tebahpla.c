a067cc23c12d646773fd1d2206657c5475653680
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-09 20:18:41 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     *
+ *      **/
+/* printing */
+int main(void)
+{
+	/* main function */
+	int i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
