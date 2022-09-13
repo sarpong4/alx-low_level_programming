ef0ba2da2cc44f0b917d1a5e2c2fc1a4e1a256f8
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-13 16:51:04 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Return: Always 0 (Success)
+ *     *     *     *
+ *      *      *      **/
+/* printing */
+void print_alphabet(void)
+{
+	/* main  */
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
