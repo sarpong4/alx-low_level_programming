e4a6d22adf7a66a0ed6dfd17e02cacd57638c22d
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2022-09-14 02:06:22 +0000

    print to 98!

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 			printf("%d, ", n);
 	}
 	else
-		for (n = n; n >= 98; n--)
+		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-	printf("\n");
+	printf("98\n");
 }
