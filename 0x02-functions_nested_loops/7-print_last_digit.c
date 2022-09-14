a29b020a9c7a41f1df13d96c38709eec9ff963b5
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2022-09-14 01:26:28 +0000

    last last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-
 	n = n % 10;
-	_putchar(n + '0');
-	return (n);
+
+	if (n < 0)
+		n = -n;
+		_putchar(n + '0');
+		return (n);
 }
