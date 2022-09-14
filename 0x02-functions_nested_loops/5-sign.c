cd3b60e24d3352791dd7fbca06ce7c0ec3cd8c79
Onesimus Wiafe <sarpnissi4@gmail.com>
2022-09-13 17:47:25 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: int value of character to be checked.
+ *
+ * Return: Always 0 (Success)
+ */
+/* printing */
+int print_sign(int n)
+{
+	/* main  */
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
