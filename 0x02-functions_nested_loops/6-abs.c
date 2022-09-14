6d5a0da0420ac7279824996ca4994d084be03aeb
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2022-09-14 01:04:24 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ *@n: number being tested
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+  if ( n >= 0)
+    return (n);
+  return (-n);
+}
