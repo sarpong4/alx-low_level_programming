d2629a77b615ecc32a868cde3f9fa18036188e21
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2022-09-16 10:15:03 +0000

    isidigit

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 	char c;
