3150ea105baaa0c74b18ab78e065129bf63d0cba
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2022-09-16 08:48:20 +0000

    more functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,5 +15,15 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int, int);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
 
 #endif
