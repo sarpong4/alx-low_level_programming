378877b44ef43d798a0f7a39d39f389373b6403c
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-04 18:28:52 +0000

    Update 0-strcat.c

--- 0x06-pointers_array_strings/0-strcat.c
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
  * _strcat - concats two arrays
- *
  * @dest: destination of concat
  * @src: source array to concat
- *
  * Return: char value
  */
 char *_strcat(char *dest, char *src)
