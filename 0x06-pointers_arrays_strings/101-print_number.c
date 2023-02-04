e1b80a3d68f2a9197aedda2b4c00716eeae5a506
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-04 18:51:07 +0000

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,16 @@
 
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
+		putchar('-');
+		num = -num;
 	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	 putchar((num % 10) + '0');
 }
