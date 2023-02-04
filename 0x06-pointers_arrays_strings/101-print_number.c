3d6bf0331f758482854cc7715ad6f970d07b209c
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-04 18:52:37 +0000

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/**
- * print_number - print int
- * @n: int parameter
- */
 
+/**
+ * print_number - prints an integer
+ * @n:integer to be printed
+ * Return: nothing
+ */
 void print_number(int n)
 {
 	unsigned int num = n;
