65715f10a97ad0a90c429d0ef17a9a3ec56ae6ca
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-04 18:41:32 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,29 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int looper1 = 0;
-	int looper2 = 0;
+	int i = 0, diff = 0;
 
-	while (s1[looper1] != '\0' && looper2 == 0)
+	while (1)
 	{
-		looper2 = s1[looper1] - s2[looper1];
-		looper1++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	return 
+	return (diff);
+}
