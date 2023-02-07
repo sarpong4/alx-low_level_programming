589e5058b43057894d3fc19b1d49106bf2d78ed0
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-07 10:44:48 +0000

    update main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int check_palindrome(char *s, int len, int index);
 int factorial(int n);
-void _print_rev_recursion(char *s);
+int find_strlen(char *s);
+int find_sqrt(int num, int root);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+void iterate_wild(char **wildstr);
+int strlen_no_wilds(char *str);
+int wildcmp(char *s1, char *s2);
+char *postfix_match(char *str, char *postfix);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
 
 #endif
