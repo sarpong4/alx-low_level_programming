c1b7b7266f2a430372d18e3b664dda9a3d92bad2
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-07 11:09:32 +0000

    primal

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 1.
+ *         If the number is not divisible - 0.
+ */
+
+int is_divisible(int num, int div)
+{
+  int x;
+  
+	if (num % div == 0)
+		return (1);
+  
+  x = _sqrt_recursion(num);
+  
+  if (x == -1)
+    x = num / 2;
+
+	if (div == x)
+		return (0);
+
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The number to be checked.
+ *
+ * Return: If the number is prime - 1.
+ *         If the number is not - 0.
+ */
+
+int is_prime_number(int n)
+{
+  int div;
+  
+  div = 2;
+  if (is_divisible(n, div))
+    return (0);
+  else
+    return (1);
+}
