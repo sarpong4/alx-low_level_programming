4413c45d7d8c98d1738065353c2c13e369aafd43
Onesimus Wiafe <sarpnissi4@gmail.com>
2023-02-07 02:23:58 -0800

    esrever

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: the string to be reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int x = 
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
