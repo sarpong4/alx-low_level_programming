a98fed3fda33d9ad8e969b38423162d977d781cd
Onesimus Sarpong Wiafe <33718928+sarpong4@users.noreply.github.com>
2023-02-07 13:24:06 +0000

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,16 +27,15 @@ int find_strlen(char *s)
  */
 void _str_reverse(char *s)
 {
-	int l, i;
-	char temp;
-	
-	l = find_strlen(s);
-	for (i = 0; i < l / 2; ++i)
+	while (*s != '\0')
+	       s++;
+	s = s- 1;
+	while (*s != '\0')
 	{
-		temp = s[i];
-		s[i] = s[l-1-i];
-		s[l-1-i] = temp;
+		putchar(*s);
+		s--;
 	}
+	
 }
 
 /**
