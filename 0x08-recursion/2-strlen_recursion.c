e2b3ef5d6ca9060d3175ee29c7ed34a5db2a0359
Onesimus Wiafe <sarpnissi4@gmail.com>
2023-02-07 02:34:21 -0800

    betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 /**
  * _strlen_recursion - return the length of a string
  * @s: string whose length will be calculated
- * 
+ *
  * Return: the length(int) of the string
  */
 
@@ -9,10 +9,10 @@ int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
