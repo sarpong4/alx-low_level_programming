c346d5035a4cdf7fc4180486329d5b90d1b1ad66
Onesimus Wiafe <sarpnissi4@gmail.com>
2023-02-08 04:32:42 -0800

    say my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - printss the name of this program
+ * @argc: number of arguments passed into main
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
