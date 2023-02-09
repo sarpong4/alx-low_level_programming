66f368978bb9f6e9122e12fa17a1efd4d583c192
Onesimus Wiafe <sarpnissi4@gmail.com>
2023-02-09 03:12:46 -0800

    args only

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,15 @@
-#include <sstdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: number of arguments passed
+ * @argv: arguments passed
+ *
+ * Return: always (0)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
